9bafc9ee289000c61d6305d8c358981b338319d9
Ricardo Leal <ricardo.leal@ill.fr>
2012-12-09 22:44:41 +0100

    ui components

--- form.py
+++ form.py
@@ -2,22 +2,12 @@
 
 # Form implementation generated from reading ui file 'form.ui'
 #
-# Created: Sat Sep 15 19:03:24 2012
+# Created: Sun Dec  9 22:43:02 2012
 #      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
-
-__author__ = "Ricardo M. Ferraz Leal"
-__copyright__ = "Copyright 2012, European Synchrotron Radiation Facility"
-__credits__ = ["Ricardo M. Ferraz Leal", "Alexander N. Popov", "Gleb P. Bourenkov",
-                    "Sean McSweeney", "Rita Giordano"]
-__license__ = "GPL"
-__version__ = "0.1"
-__maintainer__ = "Ricardo M. Ferraz Leal"
-__email__ = "ricardo.leal@esrf.fr"
-__status__ = "Beta"
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -46,7 +36,12 @@ class Ui_MainWindow(object):
         self.pushButtonRemainingDSsPattern.setGeometry(QtCore.QRect(600, 210, 191, 27))
         self.pushButtonRemainingDSsPattern.setObjectName(_fromUtf8("pushButtonRemainingDSsPattern"))
         self.table = QtGui.QTableWidget(self.centralwidget)
-        self.table.setGeometry(QtCore.QRect(10, 160, 581, 231))
+        self.table.setGeometry(QtCore.QRect(10, 160, 571, 231))
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(2)
+        sizePolicy.setHeightForWidth(self.table.sizePolicy().hasHeightForWidth())
+        self.table.setSizePolicy(sizePolicy)
         self.table.setObjectName(_fromUtf8("table"))
         self.table.setColumnCount(3)
         self.table.setRowCount(0)
@@ -74,7 +69,7 @@ class Ui_MainWindow(object):
         self.label_2.setGeometry(QtCore.QRect(110, 100, 161, 16))
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.pushButtonProcessRefDS = QtGui.QPushButton(self.centralwidget)
-        self.pushButtonProcessRefDS.setGeometry(QtCore.QRect(650, 110, 141, 27))
+        self.pushButtonProcessRefDS.setGeometry(QtCore.QRect(630, 110, 161, 27))
         self.pushButtonProcessRefDS.setObjectName(_fromUtf8("pushButtonProcessRefDS"))
         self.pushButtonProcessAllDS = QtGui.QPushButton(self.centralwidget)
         self.pushButtonProcessAllDS.setGeometry(QtCore.QRect(600, 360, 191, 27))
@@ -83,7 +78,7 @@ class Ui_MainWindow(object):
         self.textBrowser.setGeometry(QtCore.QRect(10, 400, 781, 251))
         self.textBrowser.setObjectName(_fromUtf8("textBrowser"))
         self.lineEditResolution = QtGui.QLineEdit(self.centralwidget)
-        self.lineEditResolution.setGeometry(QtCore.QRect(710, 330, 81, 25))
+        self.lineEditResolution.setGeometry(QtCore.QRect(720, 330, 71, 25))
         self.lineEditResolution.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.lineEditResolution.setObjectName(_fromUtf8("lineEditResolution"))
         self.label_3 = QtGui.QLabel(self.centralwidget)
@@ -143,25 +138,15 @@ class Ui_MainWindow(object):
         self.pushButtonCellDetails.setText(QtGui.QApplication.translate("MainWindow", "Get Cell details", None, QtGui.QApplication.UnicodeUTF8))
         self.label.setText(QtGui.QApplication.translate("MainWindow", "Space Group", None, QtGui.QApplication.UnicodeUTF8))
         self.label_2.setText(QtGui.QApplication.translate("MainWindow", "Unit Cell Parameters", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButtonProcessRefDS.setText(QtGui.QApplication.translate("MainWindow", "Process Ref Dataset", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButtonProcessAllDS.setText(QtGui.QApplication.translate("MainWindow", "Process all Datasets", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButtonProcessRefDS.setText(QtGui.QApplication.translate("MainWindow", "Process Ref Data set", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButtonProcessAllDS.setText(QtGui.QApplication.translate("MainWindow", "Process all Data sets", None, QtGui.QApplication.UnicodeUTF8))
         self.lineEditResolution.setText(QtGui.QApplication.translate("MainWindow", "2.0", None, QtGui.QApplication.UnicodeUTF8))
         self.label_3.setText(QtGui.QApplication.translate("MainWindow", "Resolution up to:", None, QtGui.QApplication.UnicodeUTF8))
         self.label_4.setText(QtGui.QApplication.translate("MainWindow", "Friedel\'s law", None, QtGui.QApplication.UnicodeUTF8))
         self.radioButtonFriedelTrue.setText(QtGui.QApplication.translate("MainWindow", "True", None, QtGui.QApplication.UnicodeUTF8))
         self.radioButtonFriedelFalse.setText(QtGui.QApplication.translate("MainWindow", "False", None, QtGui.QApplication.UnicodeUTF8))
         self.label_5.setText(QtGui.QApplication.translate("MainWindow", "Working folder:", None, QtGui.QApplication.UnicodeUTF8))
-        self.checkBoxNedit.setText(QtGui.QApplication.translate("MainWindow", "Edit with Nedit?", None, QtGui.QApplication.UnicodeUTF8))
+        self.checkBoxNedit.setText(QtGui.QApplication.translate("MainWindow", "Edit XSCALE.INP?", None, QtGui.QApplication.UnicodeUTF8))
         self.menuHelp.setTitle(QtGui.QApplication.translate("MainWindow", "Help", None, QtGui.QApplication.UnicodeUTF8))
         self.actionAbout.setText(QtGui.QApplication.translate("MainWindow", "About", None, QtGui.QApplication.UnicodeUTF8))
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtGui.QApplication(sys.argv)
-    MainWindow = QtGui.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
 
